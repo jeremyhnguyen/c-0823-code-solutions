9c6e5a753ed8df8b3aa31a278b85a5feda4e2cea
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-09-01 21:06:49 +0000

    WIP

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,8 @@
 /* exported compact */
+// function compact(array) {
+//   /
+// }
+
+// omits from array: false, null, NaN, 0, -0, undefined, empty strings
+// maintains order of truthy elements
+//  banMethods(compact, 'slice', 'splice', 'pop', 'shift', 'filter');
