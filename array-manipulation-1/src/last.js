4f4b41ad913652daf20705356a614b61f7f04a30
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-09-01 18:29:14 +0000

    WIP

---
+++ array-manipulation-1/src/last.js
@@ -0,0 +1,5 @@
+/* exported last */
+function last(array) {
+  const last = array[array.length - 1];
+  return last;
+}
---
+++ array-manipulation-1/src/tail.js
@@ -0,0 +1,9 @@
+/* exported tail */
+function tail(array) {
+  const newArr = [];
+  const no0 = array.shift(0);
+
+  newArr.push(no0);
+
+  return newArr;
+}
