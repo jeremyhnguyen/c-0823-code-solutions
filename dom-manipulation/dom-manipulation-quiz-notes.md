83d772d17fbf9922fbcee9b6b1a66c609cabba4d
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-09-12 17:23:48 +0000

    completed dom-manipulation

--- dom-manipulation/dom-manipulation-quiz-notes.md
+++ dom-manipulation/dom-manipulation-quiz-notes.md
@@ -5,18 +5,19 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - What is the `className` property of element objects?
-
+  property to retrieve or set element's class
 - How do you update the CSS class attribute of an element using JavaScript?
-
+  className, classList, setAttribute
 - What is the `textContent` property of element objects?
-
+  represents textContent a class currently has
 - How do you update the text within an element using JavaScript?
-
+  assign a new value to textContent property
 - Is the `event` parameter of an event listener callback always useful?
-
+  no
 - Would this assignment be simpler or more complicated if we didn't use a variable to keep track of the number of clicks?
-
+  more complicated
 - Why is storing information about a program in variables better than only storing it in the DOM?
+  easier to reuse the variable
 
 ## Notes
 
--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,20 +1,28 @@
 let clickNumber = 0;
 
 const $hotButton = document.querySelector('.hot-button');
-// const $clickCount = document.querySelector('.click-count');
-
-// function clicks(event) {
-//   console.log('button clicked');
-//   console.log('event:', event);
-//   console.log('event target:', event.target);
-//   clickNumber++;
-// }
-
-function userClicks($clickCount) {
-  clickNumber++;
-}
+const $clickCount = document.querySelector('.click-count');
 
 $hotButton.addEventListener('click', userClicks);
 
-console.log(clickNumber);
+function userClicks(event) {
+  clickNumber++;
+  let temp;
+  if (clickNumber < 4) {
+    temp = 'cold';
+  } else if (clickNumber < 7) {
+    temp = 'cool';
+  } else if (clickNumber < 10) {
+    temp = 'tepid';
+  } else if (clickNumber < 13) {
+    temp = 'warm';
+  } else if (clickNumber < 16) {
+    temp = 'hot';
+  } else {
+    temp = 'nuclear';
+  }
+  $hotButton.className = 'hot-button ' + temp;
+  $clickCount.textContent = 'Clicks:' + clickNumber;
+}
+
 // cool/tepid/warm/hot/nuclear
