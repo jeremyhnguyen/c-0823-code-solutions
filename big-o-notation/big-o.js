1f0e6f699c92a84ea5674285bd409a01bfba4a89
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-11 16:15:36 +0000

    completed big-o-notation

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -6,13 +6,13 @@ function uniqueLinear(words) {
   for (
     let i = 0; // 1 * 1 = O(1)
     i < words.length; // 2 * n = O(2n) ~=(n)
-    i++ // 1 * n = O(n)
+    i++ // 2 * n = O(2n) ~=(n)
   ) {
-    const word = words[i]; // 1 * n = O(n)
+    const word = words[i]; // 2 * n = O(2n) ~=(n)
     if (!seen[word]) {
-      // 1 * n = O(n)
-      seen[word] = true; // 1 * n = O(n)
-      unique[unique.length] = word; // 1 * n = O(n)
+      // 2 *  n =O(2n) ~=(n)
+      seen[word] = true; // 2 * n = O(2n) ~=(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~=(n)
     }
   }
   return unique; // 1 * 1 = O(1)
@@ -22,24 +22,24 @@ function uniqueQuadratic(words) {
   const unique = []; // 1 * 1 = O(1)
   for (
     let i = 0; // 1 * 1 = O(1)
-    i < words.length; // 2 * n = O(2n) ~=(n)
-    i++ // 1 * n = O(n)
+    i < words.length; // 2 * n = O(2n)
+    i++ // 2 * n = O(n)
   ) {
-    const word = words[i]; // 1 * n = O(n)
-    let isUnique = true; // 1 * 1 = O(1)
+    const word = words[i]; // 2 * n = O(2n)
+    let isUnique = true; // 1 * n = O(n)
     for (
-      let c = 0; // 1 * 1 = O(1)
-      c < i; // n * n = O(n^2)
-      c++ // 1 * n = O(n)
+      let c = 0; // 1 * n = O(n)
+      c < i; // 1 * n^2 = O(n^2)
+      c++ // 2 * n^2 = O(n^2)
     ) {
-      const comparing = words[c]; // 1 * n = O(n)
+      const comparing = words[c]; // 2 * n^2 = O(n^2)
       if (comparing === word) {
-        // 1 * 1 = O(1)
-        isUnique = false; // 1 * 1 = O(1)
+        // 1 * n^2 = O(n^2)
+        isUnique = false; // 1 * n^2 = O(n^2)
       }
     }
     if (isUnique) {
-      // 1 * 1 = O(1)
+      // 1 * n = O(n)
       unique[unique.length] = word; // 2 * n = O(2n)
     }
   }
