5ddc269610a4f8ad5eacd0dedd9b9990a9c40765
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-20 18:31:12 +0000

    WIP

---
+++ express-delete/server.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+});
+
+app.listen(port, () => {
+  console.log(`app listening at port ${port}`);
+});
