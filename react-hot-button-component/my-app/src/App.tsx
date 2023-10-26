067a0421f53f65fa9a9c17fae892a48f2d71fd1b
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-26 23:35:17 +0000

    removed comments from App.tsx

--- react-hot-button-component/my-app/src/App.tsx
+++ react-hot-button-component/my-app/src/App.tsx
@@ -2,12 +2,6 @@ import { useState } from 'react';
 import './App.css';
 import Button from './Button.tsx';
 
-// CLASSES: cold/cool/tepid/warm/hot/nuclear
-
-// 1.component & layout
-// 2.state
-// 3.event
-
 function App() {
   const [count, setCount] = useState(0);
   function handleClick() {
